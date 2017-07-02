331609dfb16d9550bc788412eeeb9acbf22cc50f
Benjamin Rotendahl <Benjamin@Rotendahl.dk>
2017-07-03 01:09:56 +0200

    Fixed setup

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,6 +6,11 @@ $(function() {
 });
 // Need this to show animation when go back in browser
 window.onunload = function() {};
+window.onpageshow = function(event) {
+    if (event.persisted) {
+        window.location.reload()
+    }
+};
 
 // Add lightbox class to all image links
 $("a[href$='.jpg'],a[href$='.jpeg'],a[href$='.JPG'],a[href$='.png'],a[href$='.gif']").addClass("image-popup");
@@ -50,7 +55,7 @@ $(document).ready(function() {
       tError: '<a href="%url%">Image #%curr%</a> could not be loaded.',
     },
     removalDelay: 300, // Delay in milliseconds before popup is removed
-    // Class that is added to body when popup is open. 
+    // Class that is added to body when popup is open.
     // make it unique to apply your CSS animations just to this exact popup
     mainClass: 'mfp-fade'
   });
